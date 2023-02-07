985f8cdeb418c6fe9e5180fe235af6d42cdbe3a8
MHammad-100 <hammadmansha100@gmail.com>
2023-02-07 12:18:57 +0500

    String undefined issue fixed

--- frontend/src/pages/admin/Admin.js
+++ frontend/src/pages/admin/Admin.js
@@ -21,9 +21,9 @@ const Admin = () => {
   const [currentRoute, setCurrentRoute] = useState();
 
   useEffect(()=>{
-    let path = location.pathname?.replace("/admin/","")?.replace("-", " ");
+    let path = location.pathname?.replace(/(\/)/g, "")?.replace("admin", "")?.replace("-", "");
     setCurrentRoute(path || "dashbaord");
-  }, [location.pathname]);
+  }, [location.pathname,]);
 
   const toggleCollapsed = () => {
     setCollapsed(!collapsed);
