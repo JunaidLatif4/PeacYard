b6d834e1437dcd5244e1f083c2d45f82e4d71583
MHammad-100 <hammadmansha100@gmail.com>
2023-02-09 18:55:49 +0500

    Design page added

--- frontend/src/pages/admin/pages/reports/Reports.js
+++ frontend/src/pages/admin/pages/reports/Reports.js
@@ -1,8 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Reports.scss";
 import Table from "../../../../components/table/Table";
 
 const Reports = () => {
+
+    const [reports, setReports] = useState([
+        {
+            id: 1,
+            name: "Abby Adams",
+            location: `Sales Office 1`,
+            savedDesigns: "23",
+            offersPending: "6",
+            ordersPending: "11",
+            ordersClosed: "41",
+            potentialValue: "641 989 kr",
+            closedValue: "1 233 988 kr",
+        },
+        {
+            id: 2,
+            name: "Abby Adams",
+            location: `Sales Office 1`,
+            savedDesigns: "23",
+            offersPending: "6",
+            ordersPending: "11",
+            ordersClosed: "41",
+            potentialValue: "641 989 kr",
+            closedValue: "1 233 988 kr",
+        },
+        {
+            id: 3,
+            name: "Abby Adams",
+            location: `Sales Office 1`,
+            savedDesigns: "23",
+            offersPending: "6",
+            ordersPending: "11",
+            ordersClosed: "41",
+            potentialValue: "641 989 kr",
+            closedValue: "1 233 988 kr",
+        },
+        {
+            id: 4,
+            name: "Abby Adams",
+            location: `Sales Office 1`,
+            savedDesigns: "23",
+            offersPending: "6",
+            ordersPending: "11",
+            ordersClosed: "41",
+            potentialValue: "641 989 kr",
+            closedValue: "1 233 988 kr",
+        },
+        {
+            id: 5,
+            name: "Abby Adams",
+            location: `Sales Office 1`,
+            savedDesigns: "23",
+            offersPending: "6",
+            ordersPending: "11",
+            ordersClosed: "41",
+            potentialValue: "641 989 kr",
+            closedValue: "1 233 988 kr",
+        },
+    ])
 
     const columns = [
         {
@@ -50,60 +108,16 @@ const Reports = () => {
     return (
         <>
             <Table
-                rows={[
-                    {
-                        name: "Abby Adams",
-                        location: `Sales Office 1`,
-                        savedDesigns: "23",
-                        offersPending: "6",
-                        ordersPending: "11",
-                        ordersClosed: "41",
-                        potentialValue: "641 989 kr",
-                        closedValue: "1 233 988 kr",
-                    },
-                    {
-                        name: "Abby Adams",
-                        location: `Sales Office 1`,
-                        savedDesigns: "23",
-                        offersPending: "6",
-                        ordersPending: "11",
-                        ordersClosed: "41",
-                        potentialValue: "641 989 kr",
-                        closedValue: "1 233 988 kr",
-                    },
-                    {
-                        name: "Abby Adams",
-                        location: `Sales Office 1`,
-                        savedDesigns: "23",
-                        offersPending: "6",
-                        ordersPending: "11",
-                        ordersClosed: "41",
-                        potentialValue: "641 989 kr",
-                        closedValue: "1 233 988 kr",
-                    },
-                    {
-                        name: "Abby Adams",
-                        location: `Sales Office 1`,
-                        savedDesigns: "23",
-                        offersPending: "6",
-                        ordersPending: "11",
-                        ordersClosed: "41",
-                        potentialValue: "641 989 kr",
-                        closedValue: "1 233 988 kr",
-                    },
-                    {
-                        name: "Abby Adams",
-                        location: `Sales Office 1`,
-                        savedDesigns: "23",
-                        offersPending: "6",
-                        ordersPending: "11",
-                        ordersClosed: "41",
-                        potentialValue: "641 989 kr",
-                        closedValue: "1 233 988 kr",
-                    },
-                ]}
-                hasEditDeleteAction={true}
+                rows={reports}
                 columns={columns}
+                hasEditDeleteAction
+                hasEndDate
+                hasStartDate
+                hasSearch
+                hasDropDownAction
+                dropDownActionList={["Last 30 Days", "Last Month"]}
+                tableLeftActions={["Users", "Groups", "Site id's", "Companies"]}
+                tableLeftActiveAction="Users"
             />
         </>
     )
